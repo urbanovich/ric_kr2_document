a6bb652e97680b6ba1c3c8c6e5a182fbf2196047
Urbanovich Dzmitry <urbanovich.mslo@gmail.com>
2019-12-15 13:15:52 +0300

    create library for ric kr2

---
+++ src/study/ejb/document/DocumentRemote.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package study.ejb.document;
+
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author dzmitry
+ */
+@Remote
+public interface DocumentRemote {
+    
+}
